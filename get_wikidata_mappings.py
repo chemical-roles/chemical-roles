99e527fee6f6cd45d40e3c41f163f505559d511e
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-11-04 00:13:52 +0100

    Update wikidata mapping script

--- get_wikidata_mappings.py
+++ get_wikidata_mappings.py
@@ -1,25 +1,23 @@
 # pip install sparqlwrapper
 # https://rdflib.github.io/sparqlwrapper/
 
-from SPARQLWrapper import SPARQLWrapper, JSON
+from SPARQLWrapper import JSON, SPARQLWrapper
 
-endpoint_url = "https://query.wikidata.org/sparql"
+WIKIDATA_ENDPOINT_URL = "https://query.wikidata.org/sparql"
 
 protein_query = """SELECT ?gene ?hgnc_id ?hgnc_symbol ?protein ?uniprot_id
-WHERE
-{
-	?gene wdt:P354 ?hgnc_id ;
-          wdt:P353 ?hgnc_symbol ;
-          wdt:P688 ?protein .
-    ?protein wdt:P352 ?uniprot_id .   
+WHERE {
+?gene wdt:P354 ?hgnc_id ;
+      wdt:P353 ?hgnc_symbol ;
+      wdt:P688 ?protein .
+?protein wdt:P352 ?uniprot_id .   
 }
 ORDER BY ?hgnc_id
 """
 
-chemical_query = """SELECT ?chemical ?chebi_id
-WHERE
-{
-	?chemical wdt:P683 ?chebi_id .
+chemical_query = """SELECT ?chebi_id ?chemical
+WHERE {
+?chemical wdt:P683 ?chebi_id .
 }
 ORDER BY ?chebi_id
 """
@@ -35,41 +33,46 @@ def get_results(endpoint_url, query):
 def main():
     with open('resources/wd_proteins.tsv', 'w') as file:
         print(
-            'gene_wikidata_id',
             'hgnc_id',
             'hgnc_symbol',
-            'protein_wikidata_id',
+            'gene_wikidata_id',
             'uniprot_id',
+            'protein_wikidata_id',
             sep='\t',
             file=file,
         )
-        protein_results = get_results(endpoint_url, protein_query)
-        for result in protein_results["results"]["bindings"]:
-            print(
-                result['gene']['value'].split('/')[-1],
-                result['hgnc_id']['value'],
+        protein_results = get_results(WIKIDATA_ENDPOINT_URL, protein_query)
+        protein_results = (
+            (
+                int(result['hgnc_id']['value']),
                 result['hgnc_symbol']['value'],
-                result['protein']['value'].split('/')[-1],
+                result['gene']['value'].split('/')[-1],
                 result['uniprot_id']['value'],
-                sep='\t',
-                file=file,
+                result['protein']['value'].split('/')[-1],
             )
-            
+            for result in protein_results["results"]["bindings"]
+        )
+        for result in sorted(protein_results):
+            print(*result, sep='\t', file=file)
+
     with open('resources/wd_chemicals.tsv', 'w') as file:
         print(
-            'wikidata_id',
             'chebi_id',
+            'wikidata_id',
             sep='\t',
             file=file,
         )
-        chemical_results = get_results(endpoint_url, chemical_query)
-        for result in chemical_results["results"]["bindings"]:
-            print(
-                result['chemical']['value'].split('/')[-1],
+        chemical_results = get_results(WIKIDATA_ENDPOINT_URL, chemical_query)
+        chemical_results = (
+            (
+                int(result['chebi_id']['value']),
                 f"CHEBI:{result['chebi_id']['value']}",
-                sep='\t',
-                file=file,
+                result['chemical']['value'].split('/')[-1],
             )
+            for result in chemical_results["results"]["bindings"]
+        )
+        for _, chebi_id, wikidata_id in sorted(chemical_results):
+            print(chebi_id, wikidata_id, sep='\t', file=file)
 
 
 if __name__ == '__main__':
