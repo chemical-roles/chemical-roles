3265d560c593193c527d4d45e2b0eed17b03a9ba
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-04-05 14:45:26 +0200

    Improve mesh curation script
    - reuse code
    - add more blacklist

--- curate_mesh_relations.py
+++ curate_mesh_relations.py
@@ -2,17 +2,28 @@
 
 """A script to help curate MeSH relations and infer new ones."""
 
-import os
-
 import click
-import pandas as pd
 from pyobo.sources import mesh
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-MESH_XREFS_PATH = os.path.join(HERE, 'resources', 'xrefs.tsv')
+from utils import get_xrefs_df
 
 BLACKLIST = {
     'D004791',  # Enzyme
+    'D000074389',  # Therapeutic Index, Drug
+    'D000075203',  # Contraindications, Drug
+    'D000076742',  # Synthetic Drugs
+    'D000078742',  # Substandard Drugs
+    'D000078903',  # Catalog, Drug
+    'D004305',  # Dose-Response Relationship, Drug
+    'D004366',  # Nonprescription Drugs
+    'D007202',  # Indicators and Reagents
+    'D007880',  # Legislation, Drug
+    'D011355',  # Prodrugs
+    'D013287',  # Street Drugs
+    'D015198',  # Designer Drugs
+    'D016147',  # Genes, Tumor Suppressor
+    'D016153',  # Genes, Suppressor
+    'D019155',  # Veterinary Drugs
 }
 
 SUFFIXES = [
@@ -35,20 +46,20 @@ SUFFIXES.extend([
 @click.command()
 def main():
     """Run the MeSH curation pipeline."""
-    xrefs_df = pd.read_csv(MESH_XREFS_PATH, sep='\t')
+    xrefs_df = get_xrefs_df()
     mesh_xrefs_df = xrefs_df[xrefs_df['source_db'] == 'mesh']
-    curated = set(mesh_xrefs_df['source_id'])
+    curated_mesh_ids = set(mesh_xrefs_df['source_id'])
 
     mesh_obo = mesh.get_obo()
     terms = {
         term.curie: (term, suffix)
         for term in mesh_obo
-        if term.identifier not in curated and term.identifier not in BLACKLIST
+        if term.identifier not in curated_mesh_ids and term.identifier not in BLACKLIST
         for suffix in SUFFIXES
         if term.name.lower().endswith(suffix)
     }
 
-    for i, (curie, (term, suffix)) in enumerate(sorted(terms.items()), start=1):
+    for i, (curie, (term, suffix)) in enumerate(sorted(terms.items(), key=lambda t: t[1][0].name), start=1):
         print('mesh', term.identifier, term.name, suffix.rstrip('s'), '?', '?', '?', '?', '?', sep='\t')
 
 
