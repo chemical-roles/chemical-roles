702818d49c3defb80c2daee474cf009a562667e1
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-04-09 18:41:51 +0200

    Refactor chebi curation script to use OBO

--- curate_chebi_relations.py
+++ curate_chebi_relations.py
@@ -7,15 +7,16 @@ import logging
 import os
 import sys
 from collections import defaultdict
-from typing import Collection, Iterable, Tuple
+from typing import Iterable, List, Mapping, Tuple
 
 import click
 import networkx as nx
 import pandas as pd
+import pyobo
 from protmapper import uniprot_client
 from protmapper.api import hgnc_id_to_up
+from pyobo import Obo
 from pyobo.sources.expasy import get_obo as get_expasy_obo
-from pyobo.utils import get_obo_graph
 from tabulate import tabulate
 
 from utils import (
@@ -35,91 +36,154 @@ SUMMARY_OUTPUT_PATH = os.path.join(EXPORT_DIRECTORY, 'relations_summary.tsv')
 
 RECLASSIFICATION_PATH = os.path.join(RESOURCES_DIRECTORY, 'reclassification.tsv')
 
-BIOCHEMICAL_ROLE_CHEBI_ID = 'CHEBI:52206'
-PATHWAY_INHIBITOR_CHEBI_ID = 'CHEBI:76932'
-ENZYME_INHIBITOR_CHEBI_ID = 'CHEBI:23924'
-AGONIST_CHEBI_ID = 'CHEBI:48705'
-INVERSE_AGONIST_CHEBI_ID = 'CHEBI:90847'
-INHIBITOR_CHEBI_ID = 'CHEBI:35222'
-ANTAGONIST_CHEBI_ID = 'CHEBI:48706'
+BIOCHEMICAL_ROLE_CHEBI_ID = '52206'
+PATHWAY_INHIBITOR_CHEBI_ID = '76932'
+ENZYME_INHIBITOR_CHEBI_ID = '23924'
+AGONIST_CHEBI_ID = '48705'
+INVERSE_AGONIST_CHEBI_ID = '90847'
+INHIBITOR_CHEBI_ID = '35222'
+ANTAGONIST_CHEBI_ID = '48706'
 BLACKLIST = [
-    'CHEBI:48001',  # protein synthesis inhibitor
+    '48001',  # protein synthesis inhibitor
+    '64106',  # protein kinase agonist
 ]
+
+expasy_obo = get_expasy_obo()
+chebi_obo = pyobo.get('chebi')
+chebi_id_to_name = pyobo.get_id_name_mapping('chebi')
 
 
 def _get_inhibitors_reclassification() -> pd.DataFrame:
     return pd.read_csv(RECLASSIFICATION_PATH, sep='\t', comment='#')
 
 
-def get_expasy_closure():
+def get_expasy_closure() -> Tuple[nx.DiGraph, Mapping[str, List[str]]]:
     """Get the ExPASy closure map."""
-    expasy_obo = get_expasy_obo()
     _graph = nx.DiGraph()
-    for term in expasy_obo.terms:
+    for term in expasy_obo:
         for parent_term in term.parents:
             _graph.add_edge(
-                (term.reference.prefix, term.reference.identifier, term.reference.identifier),
+                (term.prefix, term.identifier, term.identifier),
                 (parent_term.prefix, parent_term.identifier, parent_term.identifier),
             )
         for xref in term.xrefs:
             if xref.prefix in {'uniprot', 'prosite'}:
                 _graph.add_edge(
                     (xref.prefix, xref.identifier, xref.name),
-                    (term.reference.prefix, term.reference.identifier, term.reference.identifier),
+                    (term.prefix, term.identifier, term.identifier),
                 )
 
     rv = {
         identifier: list(nx.ancestors(_graph, (db, identifier, name)))
         for db, identifier, name in _graph
-        if db == 'ec-code'
+        if db == expasy_obo.ontology
     }
     return _graph, rv
 
 
-def get_enzyme_inhibitor_df(chebi_graph: nx.MultiDiGraph) -> pd.DataFrame:
+def propose_enzyme_modulators() -> pd.DataFrame:
     """Suggest enzyme inhibitors for curation."""
-    expasy_graph, ec_code_to_children = get_expasy_closure()
     rv = []
 
     source_db = 'chebi'
-    for chebi_id, data in chebi_graph.nodes(data=True):
-        chebi_name = data['name']
-
+    for term in chebi_obo:
         # Do this as a loop since there is at least one entry that corresponds to several EC codes
         ec_codes = []
 
         # Requested fix in https://github.com/ebi-chebi/ChEBI/issues/3651
-        if chebi_name == 'EC 1.22* (oxidoreductase acting on halogen in donors) inhibitor':
+        if term.name == 'EC 1.22* (oxidoreductase acting on halogen in donors) inhibitor':
             modulation = 'inhibitor'
             ec_codes.append('1.22.-.-')
 
         # Not sure why this has two
-        elif chebi_name == 'EC 1.1.1.34/EC 1.1.1.88 (hydroxymethylglutaryl-CoA reductase) inhibitor':
+        elif term.name == 'EC 1.1.1.34/EC 1.1.1.88 (hydroxymethylglutaryl-CoA reductase) inhibitor':
             modulation = 'inhibitor'
             ec_codes.append('1.1.1.34')
             ec_codes.append('1.1.1.88')
 
         # Requested rename in https://github.com/ebi-chebi/ChEBI/issues/3653
-        elif chebi_name == 'EC 1.11.1.11 (L-ascorbate peroxidase) inhibitors':
+        elif term.name == 'EC 1.11.1.11 (L-ascorbate peroxidase) inhibitors':
             modulation = 'inhibitor'
             ec_codes.append('1.11.1.11')
 
         # Requested typo fix in https://github.com/ebi-chebi/ChEBI/issues/3652
-        elif chebi_name == 'EC 3.5.5.1 (nitrilase) inhhibitor':
+        elif term.name == 'EC 3.5.5.1 (nitrilase) inhhibitor':
             modulation = 'inhibitor'
             ec_codes.append('3.5.5.1')
 
         # All other normal cases
-        elif chebi_name.startswith('EC '):
-            if chebi_name.endswith('inhibitor'):
+        elif term.name.startswith('EC '):
+            if term.name.endswith('inhibitor'):
                 modulation = 'inhibitor'
-            elif chebi_name.endswith('activator'):
+            elif term.name.endswith('activator'):
                 modulation = 'activator'
             else:
-                logger.warning(f'Unhandled suffix: chebi:{chebi_id} ! {chebi_name}')
+                logger.warning(f'Unhandled suffix: {term}')
                 continue
 
-            ec_code = chebi_name[len('EC '):].split()[0].replace('*', '-').rstrip('.')
+            ec_code = term.name[len('EC '):].split()[0].replace('*', '-').rstrip('.')
+
+            # Add any remaining dashes
+            for _ in range(3 - ec_code.count('.')):
+                ec_code += '.-'
+
+            ec_codes.append(ec_code)
+
+        else:
+            continue
+
+        rv.extend(
+            (
+                source_db, term.identifier, term.name, modulation, 'protein family', 'ec-code', ec_code, ec_code,
+            )
+            for ec_code in ec_codes
+        )
+
+    return pd.DataFrame(rv, columns=XREFS_COLUMNS)
+
+
+def get_enzyme_inhibitor_df(obo: Obo, add_proteins: bool = False) -> pd.DataFrame:
+    """Suggest enzyme inhibitors for curation."""
+    expasy_graph, ec_code_to_children = get_expasy_closure()
+    rv = []
+
+    source_db = 'chebi'
+    for term in obo:
+        # Do this as a loop since there is at least one entry that corresponds to several EC codes
+        ec_codes = []
+
+        # Requested fix in https://github.com/ebi-chebi/ChEBI/issues/3651
+        if term.name == 'EC 1.22* (oxidoreductase acting on halogen in donors) inhibitor':
+            modulation = 'inhibitor'
+            ec_codes.append('1.22.-.-')
+
+        # Not sure why this has two
+        elif term.name == 'EC 1.1.1.34/EC 1.1.1.88 (hydroxymethylglutaryl-CoA reductase) inhibitor':
+            modulation = 'inhibitor'
+            ec_codes.append('1.1.1.34')
+            ec_codes.append('1.1.1.88')
+
+        # Requested rename in https://github.com/ebi-chebi/ChEBI/issues/3653
+        elif term.name == 'EC 1.11.1.11 (L-ascorbate peroxidase) inhibitors':
+            modulation = 'inhibitor'
+            ec_codes.append('1.11.1.11')
+
+        # Requested typo fix in https://github.com/ebi-chebi/ChEBI/issues/3652
+        elif term.name == 'EC 3.5.5.1 (nitrilase) inhhibitor':
+            modulation = 'inhibitor'
+            ec_codes.append('3.5.5.1')
+
+        # All other normal cases
+        elif term.name.startswith('EC '):
+            if term.name.endswith('inhibitor'):
+                modulation = 'inhibitor'
+            elif term.name.endswith('activator'):
+                modulation = 'activator'
+            else:
+                logger.warning(f'Unhandled suffix: {term}')
+                continue
+
+            ec_code = term.name[len('EC '):].split()[0].replace('*', '-').rstrip('.')
 
             # Add any remaining dashes
             for _ in range(3 - ec_code.count('.')):
@@ -132,115 +196,125 @@ def get_enzyme_inhibitor_df(chebi_graph: nx.MultiDiGraph) -> pd.DataFrame:
 
         for ec_code in ec_codes:
             rv.append((
-                source_db, chebi_id, chebi_name, modulation, 'protein family', 'ec-code', ec_code, ec_code,
+                source_db, term.identifier, term.name, modulation, 'protein family', 'ec-code', ec_code, ec_code,
             ))
+            if not add_proteins:
+                continue
 
             children_ec_codes = ec_code_to_children.get(ec_code)
             if children_ec_codes is None:
-                print(f'could not find {ec_code} (for chebi:{chebi_id} ! {chebi_name})')
+                print(f'could not find {ec_code} (for {term})')
                 continue
 
             for target_db, target_id, target_name in children_ec_codes:
                 target_type = DB_TO_TYPE[target_db]
                 rv.append((
-                    source_db, chebi_id, chebi_name, modulation, target_type, target_db, target_id, target_name,
+                    source_db, term.identifier, term.name, modulation, target_type, target_db, target_id, target_name,
                 ))
 
     return pd.DataFrame(rv, columns=XREFS_COLUMNS)
 
 
-DB_TO_TYPE = {'ec-code': 'protein family', 'uniprot': 'protein', 'prosite': 'protein'}
+DB_TO_TYPE = {
+    'ec-code': 'protein family',
+    'uniprot': 'protein',
+    'prosite': 'protein',
+}
 
 
-def suggest_pathway_inhibitor_curation(graph: nx.MultiDiGraph) -> None:
+def suggest_pathway_inhibitor_curation() -> None:
     """Suggest pathway inhibitors for curation."""
-    inhibitors = get_xrefs_df()
-    curated_chebi_ids = set(inhibitors.chebi_id)
+    curated_chebi_ids = _get_curated_chebi_ids()
 
     reclassify_df = _get_inhibitors_reclassification()
     reclassify_chebi_ids = set(reclassify_df.chebi_id)
 
-    print(f'Children of {PATHWAY_INHIBITOR_CHEBI_ID} ({graph.nodes[PATHWAY_INHIBITOR_CHEBI_ID]["name"]})')
-    for node in nx.ancestors(graph, PATHWAY_INHIBITOR_CHEBI_ID):
-        if any(node in group for group in (curated_chebi_ids, reclassify_chebi_ids, BLACKLIST)):
+    print(f'Children of {PATHWAY_INHIBITOR_CHEBI_ID} ({chebi_id_to_name[PATHWAY_INHIBITOR_CHEBI_ID]})')
+    for chebi_id in chebi_obo.descendants(PATHWAY_INHIBITOR_CHEBI_ID):
+        if any(chebi_id in group for group in (curated_chebi_ids, reclassify_chebi_ids, BLACKLIST)):
             continue  # we already curated this!
-        name = graph.nodes[node]['name']
+        name = chebi_id_to_name[chebi_id]
         if name.endswith('inhibitor') and not name.startswith('EC '):
-            print(node, name)
+            print('chebi', f'CHEBI:{chebi_id}', name, 'inhibitor', '?', '?', '?', '?', sep='\t')
             results = post_gilda(name[:-len(' inhibitor')]).json()
             if results:
                 print(json.dumps(results, indent=2))
 
 
-def suggest_inhibitor_curation(graph: nx.MultiDiGraph) -> None:
+def suggest_inhibitor_curation() -> None:
     """Suggest inhibitors for curation."""
-    it = (
-        set(nx.ancestors(graph, INHIBITOR_CHEBI_ID))
-        - set(nx.ancestors(graph, PATHWAY_INHIBITOR_CHEBI_ID))
-        - set(nx.ancestors(graph, ENZYME_INHIBITOR_CHEBI_ID))
+    chebi_ids = (
+        chebi_obo.descendants(INHIBITOR_CHEBI_ID)
+        - chebi_obo.descendants(PATHWAY_INHIBITOR_CHEBI_ID)
+        - chebi_obo.descendants(ENZYME_INHIBITOR_CHEBI_ID)
     )
-    for t in _suggest_xrefs_curation(graph, INHIBITOR_CHEBI_ID, it, 'inhibitor'):
+    for t in _suggest_xrefs_curation(chebi_ids=chebi_ids, suffix='inhibitor'):
         print(*t, sep='\t')
 
 
-def suggest_agonist_curation(graph: nx.MultiDiGraph) -> None:
+def suggest_agonist_curation() -> None:
     """Suggest agonists for curation."""
-    _single_suggest(graph, AGONIST_CHEBI_ID, 'agonist')
+    _single_suggest(AGONIST_CHEBI_ID, 'agonist')
 
 
-def suggest_antagonist_curation(graph: nx.MultiDiGraph) -> None:
+def suggest_antagonist_curation() -> None:
     """Suggest antagonists for curation."""
-    _single_suggest(graph, ANTAGONIST_CHEBI_ID, 'antagonist')
+    _single_suggest(ANTAGONIST_CHEBI_ID, 'antagonist')
 
 
-def suggest_inverse_agonist_curation(graph: nx.MultiDiGraph) -> None:
+def suggest_inverse_agonist_curation() -> None:
     """Suggest inverse agonists for curation."""
-    _single_suggest(graph, INVERSE_AGONIST_CHEBI_ID, 'inverse agonist')
+    _single_suggest(INVERSE_AGONIST_CHEBI_ID, 'inverse agonist')
 
 
-def suggest_activator_curation(graph: nx.MultiDiGraph) -> None:
+def suggest_activator_curation() -> None:
     """Suggest activators for curation."""
-    _single_suggest(graph, BIOCHEMICAL_ROLE_CHEBI_ID, 'activator')
+    _single_suggest(BIOCHEMICAL_ROLE_CHEBI_ID, 'activator')
 
 
-def _single_suggest(graph, chebi_id, modulation, file=None) -> None:
-    it = set(nx.ancestors(graph, chebi_id))
-    for t in _suggest_xrefs_curation(graph, chebi_id, it, modulation):
+def _single_suggest(chebi_id: str, suffix, file=None) -> None:
+    for t in _suggest_xrefs_curation(suffix=suffix, chebi_ids=chebi_obo.descendants(chebi_id)):
         print(*t, sep='\t', file=file)
 
 
+def _get_curated_chebi_ids():
+    xrefs_df = get_xrefs_df()
+    return {
+        source_id[len('CHEBI:'):]
+        for source_db, source_id in xrefs_df[['source_db', 'source_id']].values
+        if source_db == 'chebi'
+    }
+
+
 def _suggest_xrefs_curation(
-    graph: nx.MultiDiGraph,
-    top_chebi_id: str,
-    it: Collection[str],
+    *,
     suffix: str,
+    chebi_ids: Iterable[str],
     show_missing: bool = False
 ) -> Iterable[Tuple[str, str, str, str, str, str]]:
     """Suggest curation.
 
-    :param graph:
-    :param top_chebi_id:
-    :param it:
-    :param suffix: The modulation type (e.g., inhibitor, agonist)
+    :param suffix: If the term's name doesn't end with this, skip it
     """
-    xrefs_df = get_xrefs_df()
-    curated_chebi_ids = set(xrefs_df.chebi_id)
+    curated_chebi_ids = _get_curated_chebi_ids()
 
-    if it:
-        print(f'\nChildren of {top_chebi_id} ({graph.nodes[top_chebi_id]["name"]})\n')
-    for node in it:
-        if node in curated_chebi_ids:
+    for chebi_id in chebi_ids:
+        if chebi_id in curated_chebi_ids:
             continue
-        name = graph.nodes[node]['name']
-        if not name.endswith(suffix):
+        name = chebi_id_to_name[chebi_id]
+        if not name.casefold().endswith(suffix.casefold()):
             continue
         results = post_gilda(name[:-len(suffix)].rstrip()).json()
         if results:
             for result in results:
                 term = result["term"]
-                yield node, name, suffix, term['db'].lower(), term['id'], term['entry_name']
+                yield (
+                    'chebi', f'CHEBI:{chebi_id}', name,
+                    suffix,
+                    term['db'].lower(), term['id'], term['entry_name'],
+                )
         elif show_missing:
-            yield node, name, suffix, '?', '?', '?', '?'
+            yield 'chebi', chebi_id, name, suffix, '?', '?', '?', '?'
 
 
 def get_relations_df(graph: nx.MultiDiGraph) -> pd.DataFrame:
@@ -325,24 +399,17 @@ def main(suggest: bool, debug: bool) -> None:
     # Sort the curated xrefs file
     sort_xrefs_df()
 
-    # Get the graph
-    graph = get_obo_graph('chebi')
-
-    if suggest:
-        suggest_activator_curation(graph)
-        suggest_pathway_inhibitor_curation(graph)
-        suggest_inhibitor_curation(graph)
-        suggest_agonist_curation(graph)
-        suggest_antagonist_curation(graph)
-        suggest_inverse_agonist_curation(graph)
+    if True:
+        suggest_activator_curation()
+        suggest_pathway_inhibitor_curation()
+        suggest_inhibitor_curation()
+        suggest_agonist_curation()
+        suggest_antagonist_curation()
+        suggest_inverse_agonist_curation()
+        propose_enzyme_modulators()
         return sys.exit(0)
 
-    relations_df = get_relations_df(graph)
-    enzyme_inhibitor_df = get_enzyme_inhibitor_df(graph)
-    df = pd.concat([
-        relations_df,
-        enzyme_inhibitor_df,
-    ]).drop_duplicates()
+    df = get_relations_df().drop_duplicates()
 
     columns = [
         'modulation', 'target_type', 'source_db', 'source_id', 'source_name', 'target_db', 'target_id', 'target_name',
